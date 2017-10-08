00147b8904fd4c7c1a9af07767bc55a10a0e6536
c0demark <kumarvijay336@gmail.com>
2017-10-08 11:26:43 +0530

    small changes in pom.xml files

--- ims-web/ims-web-rest/src/main/java/com/ring2poll/ims/web/rest/App.java
+++ ims-web/ims-web-rest/src/main/java/com/ring2poll/ims/web/rest/App.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Hello world!
+ * @author Vijay Kumar
  *
  */
-
 @SpringBootApplication
 public class App {
     public static void main( String[] args ) {
